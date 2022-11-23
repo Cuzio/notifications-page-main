7c1161adc02972c95b1aa3cfb3a02ca9b982d811
Maxwell Okoye <itomax19@gmail.com>
2022-11-23 16:19:15 +0100

    html, css and js

---
+++ index.js
@@ -0,0 +1,14 @@
+function read() {
+  document.getElementById("yet").style.backgroundColor = "hsl(0, 0%, 100%)";
+  document.getElementById("yet1").style.backgroundColor = "hsl(0, 0%, 100%)";
+  document.getElementById("yet2").style.backgroundColor = "hsl(0, 0%, 100%)";
+  document.getElementById("fa").style.visibility = "hidden";
+  document.getElementById("faa").style.visibility = "hidden";
+  document.getElementById("faaa").style.visibility = "hidden";
+  document.getElementById("notifyNumber").style.display = "none";
+}
+
+function yetPost() {
+  document.getElementById("yet").style.backgroundColor = "hsl(0, 0%, 100%)";
+  document.getElementById("fa").style.visibility = "hidden";
+}
